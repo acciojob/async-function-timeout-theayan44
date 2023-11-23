c03b52aaea132c09835ecea881027e0eb9a0bb8c
Ayan Maji <88241719+theayan44@users.noreply.github.com>
2023-11-23 22:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,9 @@
 const text = document.getElementById("text");
 const delay = document.getElementById("delay");
 const output = document.getElementById("output");
-async function display() {
-	
+async function display() {	
 	output.innerText = await new Promise((res, rej)=>{
 		setTimeout(res, delay.value, text.value);
 	});
 }
-display();
+// display();
